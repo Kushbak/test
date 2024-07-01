d65883dbe6de24dcd0c996abf15a2a55f7082a85
Kushbak Mamytov <mamytovkushbak@gmail.com>
2024-07-01 19:42:02 +0600

    asd

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+require('dotenv').config()
+const express = require('express')
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+
+const postRouter = require('./routes/post') 
+
+const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vy6mgfl.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_CLUSTERNAME}`;
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use('/posts', postRouter)
+
+const init = async () => {
+  try {
+    console.log(process.env.DB_PASSWORD)
+    await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('listening ' + process.env.PORT)
+    })
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+init()
---
+++ src/routes/post.js
@@ -0,0 +1,27 @@
+const Router = require('express')
+const Post = require('../models/post')
+const router = new Router()
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await Post.find({})
+    res.json(users)
+  } catch (e) {
+    console.log(e)
+    next(e)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const body = req.body
+    const post = Post.create(body)
+    res.json(post)
+  } catch(e) {
+    console.log(e)
+    next(e)
+  }
+})
+
+
+module.exports = router
